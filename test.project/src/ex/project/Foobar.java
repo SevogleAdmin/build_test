6703d37dd20f53d3d62980c4a4eb0dc062375d57
Adam Bovill <adam@sevogle.com>
2014-04-11 16:57:43 -0700

    aaa

--- test.project/src/ex/project/Foobar.java
+++ test.project/src/ex/project/Foobar.java
@@ -4,21 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
 
 public class Foobar {
     public String getText() throws IOException {
-        //        ClassLoader classLoader = Foobar.class.getClassLoader();
-        //        System.out.println("ClassLoader:" + classLoader);
-        URL myResource = this.getClass().getResource(".");
-        System.out.println("MyResource: " + myResource);
-        Package package1 = this.getClass().getPackage();
-        System.out.println("Package:" + package1);
-        loadString("/ex/project/src.txt");
-        loadString("src.txt");
+        loadString("/ex/project/folder/src.txt");
+        loadString("folder/src.txt");
         System.out.println("-");
 
-        InputStream in = this.getClass().getResourceAsStream("/ex/project/src.txt");
+        InputStream in = this.getClass().getResourceAsStream("/ex/project/folder/src.txt");
         InputStreamReader is = new InputStreamReader(in);
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(is);
