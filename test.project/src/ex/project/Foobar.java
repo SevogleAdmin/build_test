8c3a813c5cf3f9819262c272a70a2f6a9de40837
Adam Bovill <adam@sevogle.com>
2014-04-11 16:52:06 -0700

    Attempt to make it more like our build

--- test.project/src/ex/project/Foobar.java
+++ test.project/src/ex/project/Foobar.java
@@ -4,10 +4,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
 
 public class Foobar {
     public String getText() throws IOException {
-        InputStream in = this.getClass().getResourceAsStream("/test/project/src.txt");
+        //        ClassLoader classLoader = Foobar.class.getClassLoader();
+        //        System.out.println("ClassLoader:" + classLoader);
+        URL myResource = this.getClass().getResource(".");
+        System.out.println("MyResource: " + myResource);
+        Package package1 = this.getClass().getPackage();
+        System.out.println("Package:" + package1);
+        loadString("/ex/project/src.txt");
+        loadString("src.txt");
+        System.out.println("-");
+
+        InputStream in = this.getClass().getResourceAsStream("/ex/project/src.txt");
         InputStreamReader is = new InputStreamReader(in);
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(is);
@@ -20,5 +31,16 @@ public class Foobar {
         }
 
         return sb.toString();
+    }
+
+    private void loadString(String rsc) {
+        System.out.println("-");
+        System.out.println("Loading: " + rsc);
+        InputStream in = getClass().getResourceAsStream(rsc);
+        System.out.println("loaded: " + in);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("FB: " + (new Foobar()).getText());
     }
 }
